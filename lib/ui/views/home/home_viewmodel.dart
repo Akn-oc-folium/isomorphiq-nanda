1c7182cc7e222246b87ef99fec1aa4a25002510b
arithmic-satya <satyajit@foliumlabs.net>
2025-03-13 16:32:22 +0530

    Update home_viewmodel.dart

--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -9,6 +9,7 @@ import 'package:isomorph_iq/services/hive_service.dart';
 import 'package:isomorph_iq/ui/common/app_strings.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:telegram_web_app/telegram_web_app.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
 class HomeViewModel extends BaseViewModel {
@@ -16,6 +17,7 @@ class HomeViewModel extends BaseViewModel {
   final HiveService _hiveService = locator<HiveService>();
   final RouterService _routerService = locator<RouterService>();
   final DialogService _dialogService = locator<DialogService>();
+  final TelegramWebApp _telegramWebApp = TelegramWebApp.instance;
 
   DateTime? lastRedeemedDate;
   final int streakPoints = 50;
@@ -51,12 +53,15 @@ class HomeViewModel extends BaseViewModel {
       setBusy(true);
       _username = await _hiveService.retrieveData(kUserBox, kUsernameKey);
       if (_username == null) {
-        await _dialogService.showDialog(
+        var response = await _dialogService.showDialog(
           title: 'Error',
           description:
               "To get started, simply open the IsomorphIQ Bot's chat and send the '/start' command. This will activate the bot and unlock its features for you.",
           barrierDismissible: false,
         );
+        if (response?.confirmed ?? false) {
+          _telegramWebApp.close();
+        }
         throw Exception("Username is null.");
       }
       await Future.wait([
@@ -84,7 +89,7 @@ class HomeViewModel extends BaseViewModel {
   /// Load the stored redemption date from Hive.
   Future<void> _loadRedeemStatus() async {
     final storedDate =
-        await _hiveService.retrieveData(kUserBox, klastRedeemedKey);
+        await _hiveService.retrieveData(kUserBox, kLastRedeemedKey);
     if (storedDate is String) {
       lastRedeemedDate = DateTime.tryParse(storedDate);
     }
@@ -105,37 +110,71 @@ class HomeViewModel extends BaseViewModel {
     // Record that redemption happened now.
     lastRedeemedDate = DateTime.now();
     await _hiveService.storeData(
-        kUserBox, klastRedeemedKey, lastRedeemedDate!.toIso8601String());
+        kUserBox, kLastRedeemedKey, lastRedeemedDate!.toIso8601String());
 
     setBusyForObject("redeemingStreak", false);
   }
 
   Future<void> _fetchProfile() async {
-    if (_username == null) return;
     try {
       _userProfile = await _apiService.getDashboard(username: _username!);
       final userId = _userProfile?.data?.id;
-      if (userId != null) {
-        await _hiveService.storeData(kUserBox, kUserIdKey, userId);
-      }
-
-      final twitterAuthToken = _userProfile?.data?.twitterAuthToken ?? "";
-      if (twitterAuthToken.isNotEmpty) {
-        _xAuthTokenExists = true;
-        final storedXToken =
-            await _hiveService.retrieveData(kUserBox, kXTokenKey);
-        if (storedXToken != twitterAuthToken) {
-          await _hiveService.storeData(kUserBox, kXTokenKey, twitterAuthToken);
+      if (userId != null || userId!.isNotEmpty) {
+        if (!await _hiveService.containsKey(kUserBox, kUserIdKey)) {
+          await _hiveService.storeData(kUserBox, kUserIdKey, userId);
         }
-      } else {
-        _xAuthTokenExists = false;
-        await _hiveService.removeData(kUserBox, kXTokenKey);
-      }
 
-      _tweetCount = await _hiveService.retrieveData(kUserBox, kTweetCountsKey);
-      _totalEarned = _userProfile?.data?.points ?? 0;
+        await _fetchUserLevel(userLevel: _userProfile?.data?.level);
+
+        final twitterAuthToken = _userProfile?.data?.twitterAuthToken ?? "";
+        if (twitterAuthToken.isNotEmpty) {
+          _xAuthTokenExists = true;
+          final storedXToken =
+              await _hiveService.retrieveData(kUserBox, kXTokenKey);
+          if (storedXToken != twitterAuthToken) {
+            await _hiveService.storeData(
+                kUserBox, kXTokenKey, twitterAuthToken);
+          }
+        } else {
+          _xAuthTokenExists = false;
+          await _hiveService.removeData(kUserBox, kXTokenKey);
+        }
+
+        _tweetCount =
+            await _hiveService.retrieveData(kUserBox, kTweetCountsKey);
+        _totalEarned = _userProfile?.data?.points ?? 0;
+      }
     } catch (e) {
       debugPrint('Error fetching dashboard: $e');
+    }
+  }
+
+  Future<void> _fetchUserLevel({required int? userLevel}) async {
+    setBusy(true);
+    try {
+      // Retrieve the previously stored user level.
+      final storedUserLevel =
+          await _hiveService.retrieveData(kUserBox, kUserLevelKey);
+      debugPrint('Stored user level: $storedUserLevel');
+
+      // If no level is stored or the fetched level is different, update it.
+      if (storedUserLevel == null || storedUserLevel != userLevel) {
+        await _hiveService.storeData(kUserBox, kUserLevelKey, userLevel);
+        if (userLevel! > 1) {
+          await _hiveService.storeData(kUserBox, kPlayActiveKey, true);
+          debugPrint('Stored user level after storing: $storedUserLevel');
+        }
+      }
+      // If the play-active flag is not present and the user level is above 1,
+      // ensure the play button is active.
+      else if (!await _hiveService.containsKey(kUserBox, kPlayActiveKey) &&
+          userLevel! > 1) {
+        await _hiveService.storeData(kUserBox, kPlayActiveKey, true);
+      }
+    } catch (e) {
+      debugPrint('Error fetching dashboard: $e');
+    } finally {
+      setBusy(false);
     }
   }
 
